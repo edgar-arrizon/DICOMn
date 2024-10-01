33a4641271e481d6730c401b37f7e82a35eab1ac
edgar arrizon <edgar.arrizon@onemednet.com>
2024-10-01 12:01:28 -0700

    moved qr paths and output paths to a new file, constants.py

---
+++ constants.py
@@ -0,0 +1,22 @@
+# some dicom samples, more in the dcm_samples folder
+DCM_SAMPLE_AXIAL = "dcm_samples/002_S_0413/Axial_3TE_T2_STAR/2019-08-27_09_39_37.0/S868724/ADNI_002_S_0413_MR_Axial_3TE_T2_STAR__br_raw_20190828115108611_52_S868724_I1221049.dcm" 
+FIELD_MAPPING = "dcm_samples/002_S_0413/Field_Mapping/2019-08-27_09_39_37.0/S868733/ADNI_002_S_0413_MR_Field_Mapping__br_raw_20190828115108376_21_S868733_I1221062.dcm" 
+LAST_FIELD = "dcm_samples/002_S_0413/Field_Mapping/2019-08-27_09_39_37.0/S868733/ADNI_002_S_0413_MR_Field_Mapping__br_raw_20190828115137030_7_S868733_I1221062.dcm" 
+AXIAL_DTI_STRAIGHT_LAST = "dcm_samples/019_S_4367/Axial_DTI_straight/2019-05-20_10_18_12.0/S826089/ADNI_019_S_4367_MR_Axial_DTI_straight_br_raw_20190523093020094_4076_S826089_I1167911.dcm" 
+
+# the QR codes to modify
+QR_TIFF = "ONMD QR Code/VistaQR-website-www_onemednet_com-2.tiff"
+QR_PNG = "ONMD QR Code/VistaQR-website-www_onemednet_com-2.png"
+QR_JPEG = "ONMD QR Code/VistaQR-website-www_onemednet_com-2.jpeg"
+
+# the processed QR code
+PROCESSED_QR_TIFF = "processed_qr/qr_16bit.tiff"
+PROCESSED_QR_PNG = "processed_qr/qr_16bit.png"
+
+# the processed dicom (with original QR code)
+PROCESSED_DICOM_JPEG = "processed_dcm/qr_dicom_jpeg.dcm"
+PROCESSED_DICOM_TIFF = "processed_dcm/qr_dicom_tiff.dcm"
+PROCESSED_DICOM_PNG = "processed_dcm/qr_dicom_png.dcm"
+
+# the processed dicom (with processed original QR code)
+PROCESSED_QR_PNG_PASTED = "processed_dcm/processed_qr_png_dicom.dcm"
